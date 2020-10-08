86eb87c476980b5c7ce052c13a6c83b83d807c87
MartonMunkacsi <68861324+MartonMunkacsi@users.noreply.github.com>
2020-10-08 17:11:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+//This asks the user for input in the form of a pop up text box, then displays the chosen value accordingly
+function askFor() {
+    var text;
+    var yourNumber = prompt("Pick a number between 1-100.", "Type here...");
+
+    document.getElementById("numbers").innerHTML = yourNumber;
+}
+
+//This will generate a random value from 1-100
+function generate() {
+    var x = document.getElementById("randomise");
+    x.innerHTML = Math.floor((Math.random() * 100) + 1);
+}
+
+var win = yourNumber > x;
+var lose = yourNumber < x;
+
+
+function decision() {
+    if(win == true){
+        alert("You Won!")
+    } else {
+        alert("You Lose!")
+    }
+   
+}  
