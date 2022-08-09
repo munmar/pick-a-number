4bdc2d317ef941c63cfa1b3626cd3fba372410d1
Marton <marton.munkacsi09@gmail.com>
2022-08-09 18:43:34 +0100

    Added nav, modals, loader + semantic ui

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
-
 //This asks the user for input in the form of a pop up text box, then displays the chosen value accordingly
 function askFor() {
-    var text;
-    var yourNumber = prompt("Pick a number between 1-100.", "Type here...");
-
+    let yourNumber = prompt("Pick a number between 1-10.", "Type here...");
     document.getElementById("numbers").innerHTML = yourNumber;
+
+    document.getElementById('loader').classList.add('active')
+    let houseNumber = generate()
+    setTimeout(function() {
+        setTimeout(function() {
+            loader()
+        }, 1)
+        document.getElementById("randomise").innerHTML = houseNumber
+    }, 2000)
+
+    
+    scores.you.push(Number(yourNumber))
+    scores.house.push(houseNumber)
+
+    if (scores.you[0] === scores.house[0]){
+        console.log('You Win')
+    } else {
+        console.log('You lose')
+    }
 }
 
-//This will generate a random value from 1-100
-function generate() {
-    var x = document.getElementById("randomise");
-    x.innerHTML = Math.floor((Math.random() * 100) + 1);
+function loader() {
+    document.getElementById('loader').classList.remove('active')
 }
+
+
+// Generate a random number between 1-10
+function generate() {
+    let houseNumber = Math.floor((Math.random() * 10) + 1);
+    return houseNumber
+}
+
+// Modal function
+function modalOpenSettings() {
+    $('.settings')
+    .modal('show')
+};
+function modalOpenInfo() {
+    $('.info')
+    .modal('show')
+};
+
+
+// User needs to guess number via input
+// The input should then disappear after the guess and should be replaced by guess
+// Then House guess can generate automatically
+// House guess not allowed until user input
+// Evaluate answers and decide winner, reset board
+// Record Results in the side or on a popup (perhaps a little button in bottom right corner with the stats etc.)
+
+let scores = {
+    you: [],
+    house: []
+}
+console.log(scores)
